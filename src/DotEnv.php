7ad26c9158513ddaa68e0ca35d3d30b0eb41d16c
Dharman <tekiela246@gmail.com>
2020-08-20 23:19:35 +0100

    Update DotEnv.php

--- src/DotEnv.php
+++ src/DotEnv.php
@@ -16,18 +16,9 @@ class DotEnv
 	public static function load(string $path)
 	{
 		// open and parse the config file
-		$config = \parse_ini_file($path, true);
-		if (!$config) {
+		self::$config = \parse_ini_file($path, true, INI_SCANNER_TYPED);
+		if (!self::$config) {
 			throw new \Exception('No config file!');
-		}
-		foreach ($config as $key => $value) {
-			if (\is_array($value)) {
-				foreach ($value as $key2 => $value2) {
-					self::$config[\strtolower($key.'.'.$key2)] = $value2;
-				}
-			} else {
-				self::$config[\strtolower($key)] = $value;
-			}
 		}
 	}
 
@@ -39,6 +30,6 @@ class DotEnv
 	 */
 	public static function get(string $key)
 	{
-		return self::$config[\strtolower($key)] ?? null;
+		return self::$config[$key] ?? null;
 	}
 }
