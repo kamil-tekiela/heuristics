9f90f6247536ba582d6a47b785b9344b0f1f1cac
Dharman <tekiela246@gmail.com>
2020-12-23 22:34:31 +0000

    Fix typo

--- src/AnswerAPI.php
+++ src/AnswerAPI.php
@@ -302,7 +302,7 @@ class AnswerAPI {
 			if ($m = $h->lowEntropy()) {
 				$reasons[] = 'Low entropy';
 				$score += 0.5;
-				$triggers[] = ['type' => 'Low entropy', 'weight' => 2];
+				$triggers[] = ['type' => 'Low entropy', 'weight' => 0.5];
 			}
 
 			if ($reasons) {
