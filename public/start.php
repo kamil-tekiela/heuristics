0932d47c7b2eb3c17338faddeee653f8c8028514
Kamil Tekiela <tekiela246@gmail.com>
2024-10-10 12:46:39 +0100

    Update start.php

--- public/start.php
+++ public/start.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-exec('ps -eaf | grep \'[p]hp73 bot.php\' | awk \'{print $2}\'', $out);
+exec('ps -eaf | grep \'[p]hp bot.php\' | awk \'{print $2}\'', $out);
 foreach($out as $pid){
 	// exec("kill -9 $pid");
 }
@@ -12,7 +12,7 @@ if($out){
 	echo 'Already running';
 	die;
 }
-passthru('php73 bot.php > out.txt 2>out.txt & ', $ret);
+passthru('php bot.php > out.txt 2>out.txt & ', $ret);
 
 var_dump($ret);
 
