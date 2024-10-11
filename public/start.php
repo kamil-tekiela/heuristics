2eb36a4d7af4eeb7cdc8e5f997fbe816d774df22
Kamil Tekiela <tekiela246@gmail.com>
2024-10-11 21:49:48 +0100

    newlines

--- public/start.php
+++ public/start.php
@@ -5,15 +5,15 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 exec('ps -eaf | grep \'[p]hp bot.php\' | awk \'{print $2}\'', $out);
-foreach($out as $pid){
+foreach ($out as $pid) {
 	// exec("kill -9 $pid");
 }
-if($out){
-	echo 'Already running';
+if ($out) {
+	echo "Already running\n";
 	die;
 }
 passthru('php bot.php > out.txt 2>out.txt & ', $ret);
 
 var_dump($ret);
 
-echo 'Started!';
+echo "Started!\n";
